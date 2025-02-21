adb6a1392a6ff9b282c92f3bf738857b0c199f1c
Alpha4210 <alpha.gamer.2427@gmail.com>
2025-02-21 19:47:49 +0530

    rework on the whole code and make the game work properly

--- Basic_Games/Number_21_game.py
+++ Basic_Games/Number_21_game.py
@@ -6,27 +6,27 @@ max_num = 21
 while playing:
     ask = input("You want to play first or second: ")
     if ask=='first':
-        no_of_nos = int(input("Enter how many numbers you want to enter: "))
-        if no_of_nos>3:
-            print("You can't enter more than 3 numbers at once")
-        else:
-            for i in range(no_of_nos):
-                print(f"Enter {no_of_nos} numbers:")
-                user_num = input("")
-                list_user_num = user_num.split()
-                c_no_of_nos = randint(1,3)
-                list_c_nos = []
-                if int(list_user_num[-1]) == 21:
-                    if int(list_user_num[-1])==21:
-                        print("Computer won")
-                    else:
-                        print("Player won")
-                    break
+        while True:
+            no_of_nos = int(input("Enter how many numbers you want to enter: "))
+            if no_of_nos>3:
+                print("You can't enter more than 3 numbers at once")    
+        for i in range(no_of_nos):
+            print(f"Enter {no_of_nos} numbers:")
+            user_num = input("")
+            list_user_num = user_num.split()
+            c_no_of_nos = randint(1,3)
+            list_c_nos = []
+            if int(list_user_num[-1]) == 21:
+                if int(list_user_num[-1])==21:
+                    print("Computer won")
                 else:
-                    for i in range(int(list_user_num[-1]),int(list_user_num[-1])+c_no_of_nos):
-                        list_c_nos.append(i)
-                    for i in list_c_nos:
-                        print(i+1,end='\n')
+                    print("Player won")
+                break
+            else:
+                for i in range(int(list_user_num[-1]),int(list_user_num[-1])+c_no_of_nos):
+                    list_c_nos.append(i)
+                for i in list_c_nos:
+                    print(i+1,end='\n')
                 
                     
                 
